7ca564d0ae20241bb2eeb2130078c09c4350dd83
geovane <geonnave@gmail.com>
2014-11-29 21:59:49 -0200

    can draw a colored line in the bottom, where the pattern will be typed

--- lail.c
+++ lail.c
@@ -5,9 +5,19 @@
 
 int main(int argc, char *argv[])
 {
-	int ch, row, col;
+	int ch, row, col, brow, rcol;
 
 	initscr();
+	start_color();
+
+	init_pair(1, COLOR_CYAN, COLOR_BLACK);
+
+	while(1) {
+		getmaxyx(stdscr, brow, rcol);	// get bottom row
+		mvprintw(brow-1, 0, "teste");
+		mvchgat(brow-1, 0, -1, A_STANDOUT, 1, NULL);
+		refresh();
+	}
 
 	endwin();
 	return 0;

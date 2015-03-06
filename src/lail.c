243e35ca586b366a0b917eb3ea0ed754492519e1
geovane.fedrecheski <geovane.fedrecheski@lge.com>
2015-03-06 20:14:40 -0300

    adding *inotify* to read from file only when new stuff are added

--- src/lail.c
+++ src/lail.c
@@ -1,13 +1,21 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <fcntl.h>
-
 #include <unistd.h>
+#include <errno.h>
+
 #include <sys/select.h>
+#include <sys/inotify.h>
 
 #include "cmdl.h"
 #include "buffer.h"
 #include "lail.h"
+
+/* size of the event structure, not counting name */
+#define EVENT_SIZE (sizeof (struct inotify_event))
+
+/* reasonable guess as to size of 1024 events */
+#define BUF_LEN (1024 * (EVENT_SIZE + 16)
 
 /* lail.c
  * this is the file that do all the core work of lail program
@@ -28,12 +36,23 @@ void process_char_to_cmdl(char ch)
 	cmdl_put_char(ch);
 }
 
-void read_stuff()
+/* this function will 
+ * use *select* to read from stdin without blocking and 
+ * use *inotify* to read from file only when new stuff are added */
+void lail_run()
 {
 	fd_set rfds;
 	struct timeval tv;
-	int ret;
+	int ret, ifd, mfw;
 	char ch;
+
+	ifd = inotify_init();
+	if (ifd < 0)
+		perror("inotify_init");
+
+	mfw = inotify_add_watch(ifd, filename, IN_MODIFY);
+	if (mfw < 0)
+		perror("inotify add watch");
 
 	tv.tv_sec = 2;
 	tv.tv_usec = 0;
@@ -55,9 +74,3 @@ void read_stuff()
 	}
 }
 
-void lail_run()
-{
-	char *tmp = "blafdajdsfdssdfjsdklfdfkjfdsgdsddfjsdklafjsdlkfjfklkfaaflfjfd\0";
-
-	read_stuff();
-}

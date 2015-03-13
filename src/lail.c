22a5e05ea1b07b22726bb2816f2ae5a01a9edb79
geovane <geonnave@gmail.com>
2015-03-13 06:53:37 -0300

    implementing line selection for querieing later

--- src/lail.c
+++ src/lail.c
@@ -30,13 +30,7 @@ char line_buf[CMDL_MAX] = { '0' };
 int match_filter_query(char *line, int len)
 {
 	/*todo: check filter query*/
-	int i = 0;
-
-	printf(">");
-	for (i = 0; i < len; i++)
-		printf("%c", line[i]);
-	printf(", %d<\n", len);
-	return 1;
+	return (cmdl_query.cmd) ? cmdl_query.cmd == line[0] : 1;
 }
 
 void apply_cmdl()
@@ -66,32 +60,38 @@ void update_buffer()
 
 void append_buf_in_to_file_buf(char *buf_in, int len)
 {
-	int i = 0, line_len = 0;
+	int i = 0, j = 0, line_len = 0;
 	char *p = NULL;
 
 	p = buf_in;
 	line_len = 0;
-	for (i = 0; i < len && file_buf.len < FILE_BUF_MAX-1; i++, line_len++) {
-		/* todo: check file_buf.len */
+	/* this code is getting ugly, the possibilites for fixing this in the future are:
+	 * 1. try to fix its ugliness
+	 * 2. get used with it 
+	 * 3. writing such comments 
+	 * well; the following loop walks through the input from file and save 
+	 * it on an internal memory, the __memory_for_all_file_lines__
+	 * */
+	for (i = 0; i < len && file_buf.len < FILE_BUF_MAX-1; i++) {
 		file_buf.content[file_buf.len++] = buf_in[i];
 		if (buf_in[i] == '\n') {
 			if (match_filter_query(p, line_len))
-				filtered_buf.content[filtered_buf.len++] = buf_in[i];
+				/* yay! here we have found a matching line!
+				 * lets save it on a internal memory, the 
+				 * __memory_for_matching_lines__ */
+				for (j = 0; j < line_len && filtered_buf.len < FILE_BUF_MAX-1; j++)
+					filtered_buf.content[filtered_buf.len++] = *p++;
 			p = &buf_in[i+1];
 			line_len = 0;
+		} else {
+			line_len++;
 		}
 	}
 	must_update_buffer = 1;
 }
 
-void apply_cmdl()
-{
-	must_apply_cmdl = 0;
-}
-
 /* this function will 
- * use *select* to read from stdin without blocking and 
- * use *inotify* to read from file only when new stuff are added */
+ * use *select* to read from stdin and file without blocking */
 void lail_run()
 {
 	int ret, r_bytes, i;

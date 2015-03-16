60f464dc72795cb6ca4ba4b0525a26a0cbc653e6
geovane.fedrecheski <geovane.fedrecheski@lge.com>
2015-03-16 18:18:32 -0300

    yay! can now read file & stdin and emulate a filter

--- src/lail.c
+++ src/lail.c
@@ -29,13 +29,14 @@ char line_buf[CMDL_MAX] = { '0' };
 
 int match_filter_query(char *line, int len)
 {
-	/*todo: check filter query*/
+	/*todo: actually check filter query! */
 	return (cmdl_query.cmd) ? cmdl_query.cmd == line[0] : 1;
 }
 
 void apply_cmdl()
 {
-	int i = 0, line_len = 0;
+	int i = 0, j = 0, line_len = 0;
+	char *line_start = NULL;
 
 	must_apply_cmdl = 0;
 	must_update_buffer = 1;
@@ -43,10 +44,20 @@ void apply_cmdl()
 	clear();
 
 	filtered_buf.len = 0;
+	line_start = file_buf.content;
 	line_len = 0;
-	/*for (i = 0; i < file_buf.len; i++, line_len++)
-		if (match_filter_query(p, line_len))
-			filtered_buf.content[filtered_buf.len++] = file_buf.content[i];*/
+	for (i = 0; i < file_buf.len; i++) {
+		if (file_buf.content[i] == '\n') {
+			if (match_filter_query(line_start, line_len))
+				/* yay! here we have found a matching line!*/
+				for (j = 0; j <= line_len && filtered_buf.len < FILE_BUF_MAX-1; j++)
+					filtered_buf.content[filtered_buf.len++] = *line_start++;
+			line_start = &file_buf.content[i+1];
+			line_len = 0;
+		} else {
+			line_len++;
+		}
+	}
 }
 
 void update_buffer()
@@ -55,11 +66,11 @@ void update_buffer()
 	char *line_start = NULL;
 
 	buf_curr.x = buf_curr.y = 0;
-	line_start = file_buf.content;
-	for (i = 0; i < file_buf.len; i++) {
-		if (file_buf.content[i] == '\n') {
-			buffer_put_line(line_start, &file_buf.content[i]);
-			line_start = &file_buf.content[i+1];
+	line_start = filtered_buf.content;
+	for (i = 0; i < filtered_buf.len; i++) {
+		if (filtered_buf.content[i] == '\n') {
+			buffer_put_line(line_start, &filtered_buf.content[i]);
+			line_start = &filtered_buf.content[i+1];
 		}
 	}
 }
@@ -76,7 +87,7 @@ void append_buf_in_to_file_buf(char *buf_in, int len)
 	 * 2. get used with it 
 	 * 3. writing such comments 
 	 * well; the following loop walks through the input from file and save 
-	 * it on memory, the __memory_for_all_file_lines__
+	 * it on memory, the __memory_for_all_file_lines__ (file_buf.content)
 	 * */
 	for (i = 0; i < len && file_buf.len < FILE_BUF_MAX-1; i++) {
 		file_buf.content[file_buf.len++] = buf_in[i];
@@ -84,8 +95,8 @@ void append_buf_in_to_file_buf(char *buf_in, int len)
 			if (match_filter_query(line_start, line_len))
 				/* yay! here we have found a matching line!
 				 * lets save it on the program's memory, the 
-				 * __memory_for_matching_lines__ */
-				for (j = 0; j < line_len && filtered_buf.len < FILE_BUF_MAX-1; j++)
+				 * __memory_for_matching_lines__ (filtered_buf.content) */
+				for (j = 0; j <= line_len && filtered_buf.len < FILE_BUF_MAX-1; j++)
 					filtered_buf.content[filtered_buf.len++] = *line_start++;
 			line_start = &buf_in[i+1];
 			line_len = 0;
